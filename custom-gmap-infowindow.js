851de8102caea407ca1f87b899709f8bde65a1b2
Jonathan Adeline <jadeline@doubletrade.com>
2017-09-19 19:00:48 +0200

    Distinguish closing and closing by user interaction

--- custom-gmap-infowindow.js
+++ custom-gmap-infowindow.js
@@ -111,10 +111,19 @@ class CustomGmapInfowindow extends Polymer.GestureEventListeners(Polymer.Element
       this._marker.setVisible(true);
     }
     /**
-     * Fired when the infowindow is closed
+     * Fired when the infowindow is closed 
      * @event infowindow-closed
      */
     this.dispatchEvent(new CustomEvent('infowindow-closed', {}));
+  }
+
+  _handleUserClosing() {
+    this.close();
+    /**
+     * Fired when the infowindow is closed only by user interaction
+     * @event infowindow-closed
+     */
+    this.dispatchEvent(new CustomEvent('infowindow-closed-by-user', {}));
   }
 
   /**
@@ -291,7 +300,7 @@ class CustomGmapInfowindow extends Polymer.GestureEventListeners(Polymer.Element
       .getProjection()
       .fromLatLngToContainerPixel(this._marker.getPosition());
     // calculate placement
-    left = Math.round(point.x - (this._dim.card.width / 2));
+    left = Math.round(point.x - this._dim.card.width / 2);
     const carretHeight = parseInt(
       window
         .getComputedStyle(this.shadowRoot.querySelector('#mainContainer'), ':before')
